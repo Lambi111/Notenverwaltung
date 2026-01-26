9ede4228abf7aad873485f3d4b8826befe5f858f
lari <lara.loebens@gmail.com>
2026-01-26 13:39:32 +0100

    StudentTest wird jetzt ebenfalls gepusht

--- src/test/java/de/htwsaar/student/StudentTest.java
+++ src/test/java/de/htwsaar/student/StudentTest.java
@@ -1,4 +1,52 @@
 package de.htwsaar.student;
 
+import de.htwsaar.student.Student;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class StudentTest {
+    @Test
+    void vornameDarfNichtNullSein() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Student(null,"Mustermann", "PI");
+        });
+    }
+
+    @Test
+    void nachnameDarfNichtLeerSein() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Student("Max", " ", "PI");
+        });
+    }
+
+    @Test
+    void studiengangDarfNichtLeerSein() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Student("Max", "Mustermann", "");
+        });
+    }
+
+    @Test
+    void matrikelNummerIstZuBeginnNull() {
+        Student s = new Student("Max", "Mustermann", "PI");
+        assertEquals(0,s.getMatrikelnummer());
+    }
+
+    @Test
+    void testtoStringAusgabe() {
+        Student s = new Student("Max", "Mustermann", "PI");
+        String erwartet = "Student [matrikelnummer=" + s.getMatrikelnummer()
+                + ", vorname=" + s.getVorname() + ", nachname=" + s.getNachname()
+                + ", studiengang=" + s.getStudiengang() + "]";
+        assertEquals(erwartet, s.toString());
+    }
+
+    @Test
+    void NichtGleichBeiGleicherMatrikelnummerMit0() {
+        Student s1 = new Student("Max", "Mustermann", "PI");
+        Student s2 = new Student("Max", "Mustermann", "PI");
+
+        assertNotEquals(s1, s2);
+    }
+
 }
